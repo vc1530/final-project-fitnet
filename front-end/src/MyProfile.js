e1224bc418f337428f9eb09d35997f3347c39d3e
selftimersyd <84412113+selftimersyd@users.noreply.github.com>
2022-03-09 13:38:59 -0500

    edited MyProfile

---
+++ front-end/src/MyProfile.js
@@ -0,0 +1,16 @@
+import "/MyProfile.css"
+
+const MyProfile = () => {
+
+    return(
+        <main className="MyProfile">
+            <h1>My Profile</h1>
+            <img src="blank_profile.jpg"></img>
+
+            <h2>Name</h2>
+            <h2>Username</h2>
+            <p>Bio</p>
+        </main>
+    )
+
+}
