b0841fa1b445c309e8c0aebeb1ea9c189897d84f
jk6236 <jk6236@nyu.edu>
2022-03-18 18:12:20 -0400

    New Post file has been created.

---
+++ front-end/src/NewPost.js
@@ -0,0 +1,9 @@
+import "./NewPost.css"
+
+const AddWorkout = () => {
+    return (
+        <main className="default">
+            <h1>Dummy text so I can test things. Add Workout page.</h1>
+        </main>
+    )
+}
