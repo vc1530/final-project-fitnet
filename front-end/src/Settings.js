76c37ef45ae5a2c94127312463d543e36817cf17
selftimersyd <84412113+selftimersyd@users.noreply.github.com>
2022-03-17 21:35:48 -0700

    added stuff while in master on accident

---
+++ front-end/src/Settings.js
@@ -0,0 +1,12 @@
+import "./Settings.css"
+import {Link} from "react-router-dom"
+
+const Settings = () => {
+    return (
+        <main className="default">
+            <h1>Dummy text</h1>
+        </main>
+    )
+}
+
+export default Settings
