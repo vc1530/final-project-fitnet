6488a5e3e01332a68a5e9d26a8389809a380d7fb
crm584 <crm584@nyu.edu>
2022-03-20 15:46:09 -0400

    Basic add exercise done

--- front-end/src/AddWorkout.js
+++ front-end/src/AddWorkout.js
@@ -2,20 +2,23 @@ import "./AddWorkout.css"
 import AddWorkoutInfo from "./AddWorkoutInfo.js"
 import Header from "./Header"
 import Footer from "./Footer"
+import { Link } from "react-router-dom"
 
 const AddWorkout = () => {
     return (
-        
         <main className="AddWorkout">
             <Header
                 url = "./addWorkout" 
                 title = "Add Workout"
             /> 
             <AddWorkoutInfo/>
+
             <Footer
                 title = "AddWorkout"
             />
-        
+            <p>
+                <Link to='/addExercise'>Add Exercise</Link>
+            </p>
         </main>
         
         
