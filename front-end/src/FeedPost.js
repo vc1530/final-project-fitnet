c9b918ffd2d64640d958d863a927b2688b4adbf1
Vanessa <vc1530@nyu.edu>
2022-03-20 11:22:12 -0400

    added hover effect to posts

--- front-end/src/FeedPost.js
+++ front-end/src/FeedPost.js
@@ -1,11 +1,36 @@
 import "./FeedPost.css" 
+import user_database from "./mock_users.json" 
 
 const FeedPost = props => {   
 
+    let profiles = user_database; 
+
+    const user = profiles.find(x => x.username === props.username); 
+
+    if (typeof user == 'undefined') 
+        return (<main></main>)
+
     return ( 
         <main className = "Post-box">
-            <img src = {props.picture} alt = "Post" width = "300px" />
-            <p><b><a href = {"/" + props.username} className = "Post-link">{props.username}</a></b></p>
+            <img className = "Post-image" src = {props.picture} alt = "Post" />
+            <section className = "Profile-info">
+                    <img className = "Profile-image" src = {user.profile_pic} alt = "Profile" /> 
+                    <div className = "Profile-hover">
+                        <div className = "Profile-link">
+                            <b><a className = "User-link" href = {"/" + props.username} >{props.username}</a></b> 
+                        </div> 
+                    <div className = "Profile-card"> 
+                        <div className = "card-top"> 
+                            <img src = {user.profile_pic} alt = "profile" /> 
+                            <div className = "card-names">
+                                <b><p><a className = "User-link" href = {"/" + props.username}>{user.username}</a></p></b>
+                                <p>{user.name}</p> 
+                            </div> 
+                        </div>
+                        <p id = "bio">{user.bio}</p>
+                    </div>
+                </div>
+            </section>
             <p>{props.description}</p>
         </main>
     )
