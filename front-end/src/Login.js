3bfd72f54d2cb28dc8609d49dbc1eb1549f3c586
Vanessa <vc1530@nyu.edu>
2022-03-21 00:22:02 -0400

    more styling changes

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -1,45 +1,71 @@
-import "./SignUp.css"
+import "./Login.css"
 import {Link} from "react-router-dom"
 import Header from "./Header"
 import Footer from "./Footer" 
 import { useState } from 'react'
-
+import { AiOutlineLeft } from 'react-icons/ai'
 
 
 function Login() {
 
-
-    const [firstName, setfirstName] = useState('')
-    const [lastName, setlastName] = useState('')
-    const [email, setEmail] = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
     const [submitted, setSubmitted] = useState(false)
     const [fieldsFilled, setFieldsFilled] = useState(false)
 
+    const changeUsername = (e) => { 
+        setUsername(e.target.value); 
+    }
+
+    const changePassword = (e) => { 
+        setPassword(e.target.value); 
+    }
+
+    const handleSubmit = () => {
+        if (username === '' || password === '') {
+            setFieldsFilled(false)
+        } else {
+            setFieldsFilled(true)
+            setSubmitted(true)
+        }
+    }
+
+    function handleClick () { 
+        window.location.replace('Feed')
+    }
 
     return (
-        <main className="Login Page">
+        <main className="Login">
             <Header 
                 url = "./Login" 
                 title = "Login"
             />
-            <body className="SignUp-info">
+            <a id = "back-link" className = "User-link" href = "./"><AiOutlineLeft size = {'28px'} /></a>
+            <body id="Login-info" className = "Post-box">
                 <h1>Log into your FitNet Account!</h1>
-                <h2>Don't have an account? Register <Link to="/SignUp"> here</Link>!</h2>
-                <div className="form-group">
-                    <label>Username</label><br></br>
-                    <input value={username} type="text" className="form-control" placeholder="Username" /><br></br>
-                    <label>Password</label><br></br>
-                    <input value={password} type="text" className="form-control" placeholder="Password" /><br></br>
-                    <button type="button">Login</button>
-                </div>
-                <h4><Link to="/ForgotPassword">Forgot your password?</Link></h4>
+                <h2>Don't have an account? Register <a className = "User-link" href="/SignUp"> here</a>!</h2>
+                <form id="Login-form">
+                    <label>Username</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Username" 
+                        onChange = {changeUsername}
+                    />
+                    <label>Password</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Password" 
+                        onChange = {changePassword}
+                    />
+                    <h4><a className = "User-link" href ="/ForgotPassword">Forgot your password?</a></h4>
+                </form>
+                <div className = "submit-button" >
+                    <button onClick = {handleClick}>Login</button>
+                </div> 
             </body>
-            <Footer 
-                title = "Login" 
-            /> 
         </main>
     )
 }
