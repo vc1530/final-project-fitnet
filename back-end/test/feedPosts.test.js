525cc29f010b881dce3905c75b601ea4e83b8622
vc1530 <90410748+vc1530@users.noreply.github.com>
2022-12-08 10:35:12 -0500

    Update feedPosts.test.js

--- back-end/test/feedPosts.test.js
+++ back-end/test/feedPosts.test.js
@@ -19,6 +19,5 @@ describe('/GET posts', () => {
                 res.body.status.should.be.a("string") 
                 done() 
             })
-            .catch(done)
-    })
+    }).catch(done) 
 })
